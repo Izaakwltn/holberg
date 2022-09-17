b76220692e4276aef067655764f25d2fe0324949
Izaak Walton <izaakw@protonmail.com>
2022-09-16 22:55:12 -0600

    Adding seria module built with holberg

---
+++ seria/row-functions.lisp
@@ -0,0 +1,41 @@
+;;;; row-functions.lisp
+;;;;
+;;;; Copyright Izaak Walton (c) 2022
+
+(in-package :seria)
+
+;;; Transposition/Prime functions:
+
+(defun row-transpose (row interval)
+  "Transposes a row by a given interval"
+  (loop :for i :in row
+	:collect (holberg::pc-transpose i interval))) 
+
+(defmethod prime ((tone-row tone-row) root)
+  "Takes tone-row and root, returns transposed P-n prime."
+  (make-tone-row 'prime
+		 root
+		 (row-transpose (row tone-row)
+				(holberg::pc-interval
+				 (first (row tone-row))
+				 root))))
+
+(defmethod retrograde ((tone-row tone-row) root)
+  "Takes a row and a root, returns R-n retrograde."
+  (make-tone-row 'retrograde
+		 root
+		 (reverse (row (prime tone-row root)))))
+
+(defun find-index-value (row n)
+  "Finds tone N in tone-row, returns index value."
+  (loop :for i :from 0 :to 11
+	:if (equal n (nth i row))
+	  :do (return i)))
+
+(defmethod inverse ((tone-row tone-row) root)
+  "Takes a tone-row and root, returns I-n inverse."
+  (make-tone-row 'inverse root (mapcar #'(lambda (matrix-row)
+					   (nth (find-index-value (row tone-row) root))
+					   matrix-row)
+				       (rows (build-matrix tone-row)))))
+
--- seria/rows.lisp
+++ seria/rows.lisp
@@ -1,5 +1,65 @@
 ;;;; seria/rows.lisp
 ;;;;
-;;;; (Copyright Izaak Walton (c) 2021-2022
+;;;; Copyright Izaak Walton (c) 2021-2022
 
 (in-package :seria)
+
+(defun row-p (ls)
+  (and (typep ls 'holberg::pc-set)
+       (equal (length ls) 12)))
+
+(deftype row ()
+  `(satisfies row-p))
+
+(defclass tone-row ()
+  ((state :initarg :state
+	  :initform 'Prime
+	  :accessor state)
+   (root  :initarg :root
+	  :accessor root)
+   (row   :initarg :row
+	  :accessor row)))
+
+(defmethod print-object ((obj tone-row) stream)
+  (print-unreadable-object (obj stream :type t)
+    (with-accessors ((state state)
+		     (root root)
+		     (row row))
+	obj
+      (format stream "~a-~a: ~%~%{~{ ~a~}}" state root row))))
+
+(defun make-tone-row (state root row)
+  (check-type row row)
+  (make-instance 'tone-row :state state
+		           :root root
+			   :row row))
+
+(defun new-prime-row (row)
+  (check-type row row)
+  (make-tone-row 'prime (first row) row))
+
+;;; Generating randomized test-rows
+
+(defvar test-row nil)
+
+(defun random-row ()
+  "Generates a random tone row"
+  (loop :with row := nil
+	:with tones := '(0 1 2 3 4 5 6 7 8 9 10 11)
+
+	:for i :from 1 :to 12
+	:do (let ((r (nth (random (length tones))
+			  tones)))
+	      (progn (setq row (cons r row))
+		     (setq tones (remove-if #'(lambda (n)
+						(equal r n))
+					    tones))))
+	:finally (return row)))
+
+(defun test-row ()
+  "Makes a random row and assigns it to the variable TEST-ROW"
+  (setq test-row (new-prime-row (random-row))))
+	    
+
+
+
